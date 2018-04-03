b61174e5546cfdae6dd09c61de72f41781289a50
Yogev Heskia <yogev911@gmail.com>
2018-04-03 20:18:34 +0000

    main updated

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from libs import hcsr04 as ultrasonic
-from libs.mtk3339 import mtk3339 as gps
-
-from libs.gyro_compass import mpu9150 as gc
-
-my_ultrasonic = ultrasonic.getDistance()
-print my_ultrasonic
+#from libs.mtk3339 import mtk3339 as gps
+import time
+#from libs.gyro_compass import mpu9150 as gc
+my_ultrasonic = ultrasonic.init()
+while True:
+	my_ultrasonic = ultrasonic.getDistance()
+	print my_ultrasonic
+	time.sleep(1)
