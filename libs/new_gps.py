f47ec552ee350fff40587c7e74480cb5fe8eb990
Yogev Heskia <yogev911@gmail.com>
2018-04-04 20:51:39 +0300

    new gps

---
+++ libs/new_gps.py
@@ -0,0 +1,39 @@
+import serial #import pyserial library
+import Adafruit_BBIO.UART as UART #import UART Library
+UART.setup("UART1")  #Initialize UART1
+from time import sleep #import sleep library
+class GPS:                      #Create GPS class
+        def __init__(self):     #This init will run when you create a GPS object.
+                self.ser=serial.Serial('/dev/ttyAMA0',9600) #Initialize Serial Port
+                #This sets up variables for useful commands.
+                #This set is used to set the rate the GPS reports
+                UPDATE_10_sec=  "$PMTK220,10000*2F\r\n" #Update Every 10 Seconds
+                UPDATE_5_sec=  "$PMTK220,5000*1B\r\n"   #Update Every 5 Seconds
+                UPDATE_1_sec=  "$PMTK220,1000*1F\r\n"   #Update Every One Second
+                UPDATE_200_msec=  "$PMTK220,200*2C\r\n" #Update Every 200 Milliseconds
+                #This set is used to set the rate the GPS takes measurements
+                MEAS_10_sec = "$PMTK300,10000,0,0,0,0*2C\r\n" #Measure every 10 seconds
+                MEAS_5_sec = "$PMTK300,5000,0,0,0,0*18\r\n"   #Measure every 5 seconds
+                MEAS_1_sec = "$PMTK300,1000,0,0,0,0*1C\r\n"   #Measure once a second
+                MEAS_200_msec= "$PMTK300,200,0,0,0,0*2F\r\n"  #Meaure 5 times a second
+                #Set the Baud Rate of GPS
+                BAUD_57600 = "$PMTK251,57600*2C\r\n"          #Set Baud Rate at 57600
+                BAUD_9600 ="$PMTK251,9600*17\r\n"             #Set 9600 Baud Rate
+                #Commands for which NMEA Sentences are sent
+                GPRMC_ONLY= "$PMTK314,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*29\r\n" #Send only the GPRMC Sentence
+                GPRMC_GPGGA="$PMTK314,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*28\r\n"#Send GPRMC AND GPGGA Sentences
+                SEND_ALL ="$PMTK314,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0*28\r\n" #Send All Sentences
+                SEND_NOTHING="$PMTK314,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*28\r\n" #Send Nothing
+                ser.write(BAUD_57600)   #Set Baud Rate to 57600
+                sleep(1)                #Paulse
+                ser.baudrate=57600      #IMPORTANT Since change ser baudrate to match GPS
+                ser.write(UPDATE_200_msec) #Set update rate
+                sleep(1)
+                ser.write(MEAS_200_msec)  #Set measurement rate
+                sleep(1)
+                ser.write(GPRMC_GPGGA)    #Ask for only GPRMC and GPGGA Sentences
+                sleep(1)
+                ser.flushInput()          #clear buffers
+                ser.flushOutput()
+                print "GPS is Initialized" #Print message
+
--- main.py
+++ main.py
@@ -4,19 +4,35 @@ import libs.gps_module as gpsm
 # my_ultrasonic = ultrasonic.getDistance()
 # print my_ultrasonic
 # time.sleep(1)
+#
+# gps = gpsm.mt3339("/dev/ttyAMA0")
+# gps.cold_start()
+# gps.warm_start()
+# gps.hot_start()
+# gps.set_baudrate(115200)
+# gps.set_nmea_update_rate(1000)
+# gps.set_nav_speed_threshold(1.5)
+# gps.set_nmea_output(gll = 0, rmc = 1, vtg = 0, gga = 5, gsa = 5, gsv = 5)
+#
+# gps.set_fix_update_rate(800)
+# gps.set_nmea_update_rate(800)
+# gps.set_baudrate(115200)
+# gps.set_nmea_update_rate(1000)
+# gps.set_nav_speed_threshold(1.5)
+# gps.set_nmea_output(gll = 0, rmc = 1, vtg = 0, gga = 5, gsa = 5, gsv = 5)
 
-gps = gpsm.mt3339("/dev/ttyAMA0")
-gps.cold_start()
-gps.warm_start()
-gps.hot_start()
-gps.set_baudrate(115200)
-gps.set_nmea_update_rate(1000)
-gps.set_nav_speed_threshold(1.5)
-gps.set_nmea_output(gll = 0, rmc = 1, vtg = 0, gga = 5, gsa = 5, gsv = 5)
+from libs.new_gps import GPS
+myGPS=GPS()
 
-gps.set_fix_update_rate(800)
-gps.set_nmea_update_rate(800)
-gps.set_baudrate(115200)
-gps.set_nmea_update_rate(1000)
-gps.set_nav_speed_threshold(1.5)
-gps.set_nmea_output(gll = 0, rmc = 1, vtg = 0, gga = 5, gsa = 5, gsv = 5)
+while(1):
+        myGPS.ser.flushInput() #Clear Buffers
+        myGPS.ser.flushInput()
+        while myGPS.ser.inWaiting()==0: #Wait for input
+                pass
+        NMEA1=myGPS.ser.readline()      #Read NMEA1
+        while myGPS.ser.inWaiting()==0:
+                pass
+        NMEA2=myGPS.ser.readline()
+        print NMEA1
+        print NMEA2
+
