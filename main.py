887ec6d0f19e1c3c7502f500fddd24c227f608be
Yogev Heskia <Yogev911@gmail.com>
2018-04-05 15:36:06 +0300

    new gps

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import libs.mpu9150 as compass
 import libs.adafruit_i2c_interface as i2c_bus
-imu = compass.mpu9150(i2c_bus)
+imu = compass.mpu9150(i2c_bus.Device())
 print 'statring'
 print imu.read_x_acc_raw()
 print imu.read_x_acc_raw()
