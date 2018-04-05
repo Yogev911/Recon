5c693bafc74f93105c79edfb20d25e171126abda
Yogev Heskia <Yogev911@gmail.com>
2018-04-05 14:18:23 +0300

    new gps

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from libs.gyro_compass import mpu9150 as compass
 imu = compass.MPU9150(3)
+print 'statring'
 print(imu.accel.xyz)
 print(imu.gyro.xyz)
 print(imu.mag.xyz)
 print(imu.temperature)
 print(imu.accel.z)
+print 'done'
