5feaabd5e070f8df3aac30c9e4e66a8274eadf39
Yogev Heskia <Yogev911@gmail.com>
2018-04-07 18:34:14 +0300

    calc azimut

--- main.py
+++ main.py
@@ -7,20 +7,20 @@ from utils import ultrasonic_handler as us
 
 
 def main():
-    print 'gyro:'
-    gyro.get_gyro()
-    print 'accel:'
-    gyro.get_gyro()
-    # print 'magnet'
-    # gyro.get_magnet()
-    print 'temp:'
-    gyro.get_temp()
+    # print 'gyro:'
+    # gyro.get_gyro()
+    # print 'accel:'
+    # gyro.get_gyro()
+    # # print 'magnet'
+    # # gyro.get_magnet()
+    # print 'temp:'
+    # gyro.get_temp()
     print 'azimut:'
     gyro.get_azimut()
-    print 'gps:'
-    gps.get_location()
-    print 'ultra sonic:'
-    print us.getDistance()
+    # print 'gps:'
+    # gps.get_location()
+    # print 'ultra sonic:'
+    # print us.getDistance()
 
 
 def init():
@@ -31,4 +31,4 @@ if __name__ == '__main__':
     init()
     while 1:
         main()
-        sleep(0.2)
+        sleep(0.1)
