456c01161ce13522b9d866df1bf6cddc861a85a8
Yogev Heskia <Yogev911@gmail.com>
2018-06-10 20:53:53 +0300

    refuctor

--- soldier_api.py
+++ soldier_api.py
@@ -85,12 +85,12 @@ class SoldierApi():
 
     def _wait_for_hololence(self):
         print 'looking for hololence on ip {} in port {}...'.format(self.address[0], self.address[1])
-        while True:
-            # self.soldier.print_gps_data()
-            if self._ping():
-                break
-            self._spinner()
-            sleep(1)
+        # while True:
+        #     # self.soldier.print_gps_data()
+        #     if self._ping():
+        #         break
+        #     self._spinner()
+        #     sleep(1)
         print 'wating for hololence to send start msg...'
         while True:
             try:
