ea2e317490de78c3e1fd724d2af566ab7358e15b
Yogev Heskia <Yogev911@gmail.com>
2018-06-17 21:27:48 +0300

    refuctor

--- Target.py
+++ Target.py
@@ -6,7 +6,7 @@ from math import sin, cos, sqrt, atan2, radians, pi, acos, tan, atan, asin, degr
 
 from utils import gps_handler as my_gps, conf
 # from utils.lidar import GLLv3
-# from utils import ultrasonic_handler as us
+from utils import ultrasonic_handler as us
 
 spinner = itertools.cycle(['-', '/', '|', '\\'])
 
