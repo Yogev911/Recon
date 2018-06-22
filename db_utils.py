b489b664f4d750c28a6a3bf7c1058e0e51c47dc7
Yogev Heskia <yogev911@gmail.com>
2018-06-22 15:02:22 +0300

    none

--- db_utils.py
+++ db_utils.py
@@ -1,7 +1,7 @@
 import json
 from time import sleep
 
-from requests import post, get, delete
+from requests import post, get, delete, ConnectionError
 
 from settings import conf
 
@@ -14,39 +14,64 @@ class db():
         self.msg = 'message/'
 
     def send_target(self, target):
-        res = post(self.root + self.target, json=target)
-        if res.status_code != 200:
-            print 'error update db'
-            print res.json()
+        try:
+            res = post(self.root + self.target, json=target)
+            if res.status_code != 200:
+                print 'error update db'
+                print res.json()
+        except ConnectionError as e:
+            print e
 
     def get_target(self, target_id):
-        return get(self.root + self.target + target_id)
+        try:
+            return get(self.root + self.target + target_id)
+        except ConnectionError as e:
+            print e
+            return None
 
     def get_targets(self):
-        res = get(self.root + self.target)
-        if res.status_code == 200:
-            responds = json.loads(res.content)
-            if responds['success']:
-                return responds['data']
+        try:
+            res = get(self.root + self.target)
+            if res.status_code == 200:
+                responds = json.loads(res.content)
+                if responds['success']:
+                    return responds['data']
+                else:
+                    print 'host unavailable'
+                    sleep(2)
+                    return None
             else:
                 print 'host unavailable'
                 sleep(2)
                 return None
-        else:
+        except ConnectionError as e:
+            print e
+            print 'host unavailable'
+            sleep(1)
+            return None
+
+    def delete_target(self, target_id):
+        try:
+            return delete(self.root + self.target + target_id)
+        except ConnectionError as e:
+            print e
             print 'host unavailable'
             sleep(2)
             return None
 
-    def delete_target(self, target_id):
-        return delete(self.root + self.target + target_id)
-
     def gets_msg(self):
-        res = get(self.root + self.msg + str(conf.RECONUNITID))
-        if res.status_code == 200:
-            responds = json.loads(res.content)
-            if responds['success']:
-                return responds['data']
-            else:
-                print 'host unavailable'
-                sleep(2)
-                return None
+        try:
+            res = get(self.root + self.msg + str(conf.RECONUNITID))
+            if res.status_code == 200:
+                responds = json.loads(res.content)
+                if responds['success']:
+                    return responds['data']
+                else:
+                    print 'host unavailable'
+                    sleep(2)
+                    return None
+        except ConnectionError as e:
+            print e
+            print 'host unavailable'
+            sleep(1)
+            return None
